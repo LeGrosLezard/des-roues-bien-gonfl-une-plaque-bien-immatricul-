954c323bcd47cd21c6a7b0e66e96c066ce36df32
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-10-29 00:55:10 +0100

    Update main.py
    bon faut que jmatte snake mais ca parle de vitesse c quoi struk

--- background/main.py
+++ background/main.py
@@ -56,6 +56,14 @@ def make_line(thresh, size, color):
 
 
 
+def adjust_gamma(image, gamma):
+    """We add light to the video, we play with gamma"""
+
+    invGamma = 1.0 / gamma
+    table = np.array([((i / 255.0) ** invGamma) * 255
+            for i in np.arange(0, 256)]).astype("uint8")
+
+    return cv2.LUT(image, table)
 
 
 
@@ -88,161 +96,42 @@ if __name__ == "__main__":
             cv2.drawContours(blanck, [cnts], -1, (0,255,0), 1)
 
 
-        #show_picture("blanck", blanck, 0, "")
+        show_picture("blanck", blanck, 0, "")
 
         gray = cv2.cvtColor(blanck, cv2.COLOR_BGR2GRAY)
         th3 = cv2.adaptiveThreshold(gray, 255, MG, T,11,5)
         th3 = make_line(th3, 10, 255)
 
-        #show_picture("th3", th3, 0, "")
+        show_picture("th3", th3, 0, "")
 
-
-        size = 50
-        blanck_th3 = blanck_picture(img)
-
-
-        for x in range(0, th3.shape[1], size):
-            for y in range(0, th3.shape[0], size):
-
-                crop = th3[y:y+50, x:x+50]
-                crop = make_line(crop, 2, 255)
-                blanck_th = blanck_picture(crop)
-
-
-                contours, _ = cv2.findContours(crop, R, P)
-                for cnt in contours:
-                    if cv2.contourArea(cnt) < 1500 and\
-                       cv2.contourArea(cnt) > 200:
-                        cv2.drawContours(blanck_th,[cnt], -1, (255,255,255), 1)
-                        #cv2.fillPoly(blanck_th, pts =[cnt], color=(255, 255, 255))
-                        #show_picture("blanck_th", blanck_th, 0, "")
-
-                blanck_th3[y:y+50, x:x+50] = blanck_th
-
-                #show_picture("blanck_th3", blanck_th3, 0, "")
-
-
-        #show_picture("blanck_th3", blanck_th3, 0, "")
-
-        blanck4 = blanck_picture(blanck_th3)
-        gray = cv2.cvtColor(blanck_th3, cv2.COLOR_BGR2GRAY)
-
-        contours, _ = cv2.findContours(gray, R, P)
-
-
-        for cnts in contours:
-            if cv2.contourArea(cnts) > 250:
-                #print(cv2.contourArea(cnts))
-                cv2.drawContours(blanck4, [cnts], -1, (255,255,255), 1)
-                #show_picture("blanck4", blanck4, 0, "")
-
-        #show_picture("blanck4", blanck4, 0, "")
-
-
-
-        blanck6 = blanck_picture(img)
-        for x in range(0, blanck4.shape[1], size):
-            for y in range(0, blanck4.shape[0], size):
-
-                try:
-                    crop = blanck4[y:y+50, x:x+50]
-                    crop = make_line(crop, 2, 0)
-                    gray_crop = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
-
-                    crop1 = blanck4[y-50:y, x-50:x]
-                    crop1 = make_line(crop1, 2, 0)
-                    gray_crop1 = cv2.cvtColor(crop1, cv2.COLOR_BGR2GRAY)
-
-                    crop2 = blanck4[y+50:y+100, x+50:x+100]
-                    crop2 = make_line(crop2, 2, 0)
-                    gray_crop2 = cv2.cvtColor(crop2, cv2.COLOR_BGR2GRAY)
-
-
-                    c = 0; c1 = 0;
-                    for x_crop in range(0, crop.shape[1]):
-                        for y_crop in range(0, crop.shape[0]):
-                            if gray_crop1[x_crop, y_crop] != 0:   
-                                c+=1
-                            if gray_crop2[x_crop, y_crop] != 0:
-                                c1+=1
-
-                    if c == 0 and c1 == 0:
-                        pass
-                    else:
-                        blanck6[y:y+size, x:x+size] = crop
-
-
-                except:
-                    pass
-
-
-        #show_picture("blanck6", blanck6, 0, "")
-
-        gray = cv2.cvtColor(blanck6, cv2.COLOR_BGR2GRAY)
-        th3 = cv2.adaptiveThreshold(gray, 255, MG, T,11,2)
-
-        contours, _ = cv2.findContours(th3, R, P)
 
         maxi = 0
-        for cnts in contours:
-            if cv2.contourArea(cnts) > maxi:
-                maxi = cv2.contourArea(cnts)
-
-        blanck7 = blanck_picture(img)
-        blanck7 = cv2.cvtColor(blanck7, cv2.COLOR_BGR2GRAY)
-        maxi2 = 0
-        for cnts in contours:
-            if cv2.contourArea(cnts) < maxi and\
-             cv2.contourArea(cnts) > maxi2:
-                maxi2 = cv2.contourArea(cnts)
-
-        for cnts in contours:
-            if cv2.contourArea(cnts) == maxi2:
-                x, y, w, h = cv2.boundingRect(cnts)
-                blanck7[y:y+h, x:x+w] = 255
+        maxi1 = 0
+        for cnt in contours:
+            if cv2.contourArea(cnt) > maxi:
+                maxi = cv2.contourArea(cnt)
+                print(maxi)
+            
+        blanck5 = blanck_picture(img)
+        contours, _ = cv2.findContours(th3, R, P)
+        for cnt in contours:
+            if cv2.contourArea(cnt) < 145272 and\
+               cv2.contourArea(cnt) > 1000:
+                #print(cv2.contourArea(cnt))
+                cv2.fillPoly(blanck5, pts =[cnt], color=(255, 255, 255))
+        show_picture("blanck5", blanck5, 0, "")
 
 
-        for x in range(0, blanck7.shape[0]):
-            for y in range(0, blanck7.shape[1]):
-                if blanck7[x, y] == 255:
+        for x in range(0, blanck5.shape[0]):
+            for y in range(0, blanck5.shape[1]):
+                if blanck5[x, y][0] == 255 and\
+                   blanck5[x, y][1] == 255 and\
+                   blanck5[x, y][2] == 255:
                     pass
                 else:
-                    img[x, y] = 0
+                    img[x, y] = 255, 26, 100
 
-
-
-        cv2.imshow("dsq", img)
-        cv2.waitKey(0)
-        cv2.imwrite("image" + str(cc) + ".jpg", img)
-
-        cc += 1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#iscontourconvexe roue
-
-
-
-
-
-
-
-
-
-
-
+        show_picture("img", img, 0, "")
 
 
 
