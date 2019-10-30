17d92ea8e0da31bf334ee1e66cc6c33ad9584d96
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-10-30 18:32:43 +0100

    Update main3.py
    bon faut juste recommencé d'a partir les pixels

--- background/main3.py
+++ background/main3.py
@@ -9,7 +9,8 @@ import time
 import matplotlib.pyplot as plt
 from PIL import Image
 
-
+from skimage import exposure
+from skimage import feature
 
 
 def open_picture(image, mode):
@@ -68,7 +69,16 @@ def main_color_background(img):
             dico[value] += 1
         else:
             dico[value] = 1
-    return dico
+
+
+    for key, value in dico.items():
+        if value > max_value and key != (0, 255, 0):
+            max_value = value; color = key;
+
+
+    return dico, color
+
+
 
 
 
@@ -92,65 +102,148 @@ if __name__ == "__main__":
 
 
 
-    size = 10
+    size = 5
     for i in range(len(liste_image)):
 
         img = open_picture(path_image.format(liste_image[i]), 1)
+        copy = img.copy()
+
         show_picture("img", img, 0, "")
 
 
-        for nb in range(1, 4):
-            #court image
-            for x in range(0, img.shape[1], size*nb):
-                for y in range(0, img.shape[0], size*nb):
-                    a = 0; b = 0; c = 0; d = 0;
+        #court image
+        for x in range(0, img.shape[1], size):
+            for y in range(0, img.shape[0], size):
+                a = 0; b = 0; c = 0; d = 0;
 
-                    #5 par 5
-                    crop_in = img[y:y+size*nb, x:x+size*nb]
-                    #show_picture("crop_in", crop_in, 0, "")
-                    for i in crop_in.tolist():
-                        for j in i:
+                #5 par 5
+                crop_in = img[y:y+size, x:x+size]
+                #show_picture("crop_in", crop_in, 0, "")
+                for i in crop_in.tolist():
+                    for j in i:
 
-                            #moyenne
-                            a += j[0]; b+=j[1];c+=j[2]
-                            d+=1
+                        #moyenne
+                        a += j[0]; b+=j[1];c+=j[2]
+                        d+=1
 
-                    if d != 0:
+                if d != 0:
 
-                        #reatribut couleur
-                        img[y:y+size*nb, x:x+size*nb] = int(a/d), int(b/d), int(c/d)
-                        #show_picture("img", img, 0, "")
-
-            show_picture("imgpix", img, 0, "")
+                    #reatribut couleur
+                    img[y:y+size, x:x+size] = int(a/d), int(b/d), int(c/d)
+                    #show_picture("img", img, 0, "")
 
 
+        #show_picture("imgpix", img, 0, "")
 
 
 
 
 
 
+        size = 30
+
+
+        #court image
+        for x in range(0, img.shape[1], size):
+            for y in range(0, img.shape[0], size):
+                a = 0; b = 0; c = 0; d = 0;
+
+                #5 par 5
+                crop_in = img[y:y+size, x:x+size]
+                #show_picture("crop_in", crop_in, 0, "")
+                for i in crop_in.tolist():
+                    for j in i:
+
+                        #moyenne
+                        a += j[0]; b+=j[1];c+=j[2]
+                        d+=1
+
+                if d != 0:
+
+                    #reatribut couleur
+                    img[y:y+size, x:x+size] = int(a/d), int(b/d), int(c/d)
+                    #show_picture("img", img, 0, "")
+
+
+        show_picture("imgpix1", img, 0, "")
+
+
+        dico, _ = main_color_background(img)
+
+        print(dico)
+
+        liste = []
+        for i in dico:
+            liste.append(i)
+
+        liste1 = []
+        for key, value in dico.items():
+            liste1.append(value)
+
+
+        dico1 = {}
+        for i in liste:
+            dico1[i] = 0
+
+        print(dico1)
+
+
+        for i in liste:
+            for j in liste:
+                if i[0] > j[0] - 30 and\
+                   i[0] < j[0] + 30 and\
+                   i[1] > j[1] - 30 and\
+                   i[1] < j[1] + 30 and\
+                   i[2] > j[2] - 30 and\
+                   i[2] < j[2] + 30 and\
+                   i != j:
+                    for key, value in dico.items():
+                        if key == i:
+                            for key1, value1 in dico1.items():
+                                if key1 == key:
+                                    dico1[key1] = key
+                    liste.remove(j)
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        print("")        
+        print(dico1)
 
                 
+        print("")
+        print("")
+
+
+
+
+        for x in range(0, img.shape[0], size):
+            for y in range(0, img.shape[1], size):
+                for key1, value1 in dico1.items():
+
+                    if img[x, y][0] == key1[0] and\
+                       img[x, y][1] == key1[1] and\
+                       img[x, y][2] == key1[2]:
+                        if value1 == 0:
+                            img[x:x+30, y:y+30] = key1[0], key1[1], key1[2]
+
+                        else:
+                            img[x:x+30, y:y+30] = value1[0], value1[1], value1[2]
+
+                        show_picture("yoyo", img, 0, "")
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
